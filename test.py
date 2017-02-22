da5a0d6fe68600b4b05c2b0bae796dd8a441a727
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-02-22 15:12:29 -0600

    [ADD] Test

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ class TestSequenceFunctions(unittest.TestCase):
             self.assertTrue(element in self.seq)
 
     def test_upper(self):
-        self.assertEqual('Daniel'.upper(), 'DANIEL')
+        self.assertEqual('Daniel'.upper(), 'Omar')
 
 if __name__ == '__main__':
     unittest.main(
